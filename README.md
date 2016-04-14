9fa640c2a37a94c86e8066fa479a569a5c4aec18
tchiotludo <tchiot.ludo@gmail.com>
2016-04-14 17:21:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Сáша Cache
+-----
+
+## Warning
+Be aware that this package is still in heavy developpement.
+Some breaking change will occure. Thank's for your comprehension.
+
+## Features
+* Drivers
+   * Apc
+   * Redis
+* Namespace
+* Versionning of cache to destroy all the namespace
+* Tags of key in order to destroy all related keys
+* Gzip by default
+
+## About
+
+### License
+
+Cawa is licensed under the GPL v3 License - see the `LICENSE` file for details
---
+++ src/CacheFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the Сáша framework.
+ *
+ * (c) tchiotludo <http://github.com/tchiotludo>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types = 1);
+
+namespace Cawa\Cache;
+
+use Cawa\Core\DI;
+
+trait CacheFactory
+{
+    /**
+     * @var array
+     */
+    private static $container = [];
+
+    /**
+     * @param string $name
+     *
+     * @return Cache
+     */
+    private static function cache(string $name = null) : Cache
+    {
+        if ($return = DI::get(__METHOD__,  $name)) {
+            return $return;
+        }
+
+        $config = DI::config()->get('cache/' . ($name ?: 'default'));
+        $item = Cache::create($config);
+
+        return DI::set(__METHOD__,  $name, $item);
+    }
+}
